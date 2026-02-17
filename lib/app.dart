4408371f75bfb806b5d3a659546818f5cd2c3ec4
farihaarai <farihaarai@gmail.com>
2026-02-17 11:53:50 +0530

    project setup done

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class KitchenOrderMgmt extends StatelessWidget {
+  const KitchenOrderMgmt({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:kitchen_order_mgmt_app/app.dart';
+
+void main() {
+  runApp(const KitchenOrderMgmt());
+}
